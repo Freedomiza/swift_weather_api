dccb28e354e10000aa0b6b363bf2a2aefe30d254
Khoa Le <freedomiza1991@gmail.com>
2022-06-14 13:31:16 +0700

    Initial Commit

---
+++ com.my.personalproject/com_my_personalprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_my_personalprojectApp.swift
+//  com.my.personalproject
+//
+//  Created by Khoa Le on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct com_my_personalprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ com.my.personalprojectUITests/com_my_personalprojectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  com_my_personalprojectUITestsLaunchTests.swift
+//  com.my.personalprojectUITests
+//
+//  Created by Khoa Le on 6/14/22.
+//
+
+import XCTest
+
+class com_my_personalprojectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
